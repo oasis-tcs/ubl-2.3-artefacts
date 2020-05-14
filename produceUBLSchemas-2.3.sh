180cb4e0cc40e28dcf244fc6bcc81c022bffed5b
gkholman <gkenholman+github@gmail.com>
2020-05-13 21:28:29 -0400

    Comment out failing statement

--- produceUBLSchemas-2.3.sh
+++ produceUBLSchemas-2.3.sh
@@ -3,12 +3,12 @@
 if [ "$3" = "" ]
 then 
 echo Missing results directory, stage and dateZ arguments
-exit
+exit 1
 fi
 
 echo Building package...
 java -Dant.home=utilities/ant -classpath utilities/saxon/saxon.jar:utilities/ant/lib/ant-launcher.jar:utilities/saxon9he/saxon9he.jar:. org.apache.tools.ant.launch.Launcher -buildfile produceUBLschemas.xml "-Dpackage=2.3" -DUBLversion=2.3 -DUBLprevStageVersion=2.3 -DUBLprevStage=csprd02 -DUBLprevVersion=2.2 -Drawdir=raw -Ddir=$1 -Dstage=$2 -Dversion=$3 $4  | tee artefacts.console.$3.txt
-serverReturn=${PIPESTATUS[0]}
+# serverReturn=${PIPESTATUS[0]}
 
 if [ ! -d $1 ] then mkdir $1
 if [ ! -d $1/artefacts-UBL-2.3-$2-$3 ] then mkdir $1/artefacts-UBL-2.3-$2-$3
