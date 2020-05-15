e08cdba921e2f62185ece904427c5f46a9b60af2
gkholman <gkenholman+github@gmail.com>
2020-05-15 13:19:40 -0400

    Invoke bash not sh

--- produceUBLSchemas-2.3.sh
+++ produceUBLSchemas-2.3.sh
@@ -6,7 +6,7 @@ if [ "$3" = "" ]; then echo Missing results directory, stage and dateZ arguments
 
 echo Building package...
 java -Dant.home=utilities/ant -classpath utilities/saxon/saxon.jar:utilities/ant/lib/ant-launcher.jar:utilities/saxon9he/saxon9he.jar:. org.apache.tools.ant.launch.Launcher -buildfile produceUBLschemas.xml "-Dpackage=2.3" -DUBLversion=2.3 -DUBLprevStageVersion=2.3 -DUBLprevStage=csprd02 -DUBLprevVersion=2.2 -Drawdir=raw -Ddir=$1 -Dstage=$2 -Dversion=$3 $4  | tee artefacts.console.$3.txt
-# serverReturn=${PIPESTATUS[0]}
+serverReturn=${PIPESTATUS[0]}
 
 if [ ! -d $1 ]; then mkdir $1 ; fi
 if [ ! -d $1/artefacts-UBL-2.3-$2-$3 ]; then mkdir $1/artefacts-UBL-2.3-$2-$3 ; fi
