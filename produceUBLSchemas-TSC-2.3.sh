3fe5fe98c104a58e478e8fbe5683e930770df3b8
gkholman <gkenholman+github@gmail.com>
2020-07-15 21:48:07 -0400

    Recover examples

--- produceUBLSchemas-TSC-2.3.sh
+++ produceUBLSchemas-TSC-2.3.sh
@@ -15,8 +15,7 @@ export UBLprevStageVersion=2.3
 export UBLprevStage=csprd02
 export UBLprevVersion=2.2
 export rawdir=raw
-export libGoogle=https://docs.google.com/spreadsheets/d/1l1z01tb3bkEHgvC-EU8AViqxg5cHWgx1iLt1UwNFEoA #bad cells
-export libGoogle=https://docs.google.com/spreadsheets/d/1n-jNRF9iL-eoDYjU-jpoV5VnxOSU93w0O-lGGqgcnnI #repaired cells
+export libGoogle=https://docs.google.com/spreadsheets/d/1l1z01tb3bkEHgvC-EU8AViqxg5cHWgx1iLt1UwNFEoA
 export docGoogle=https://docs.google.com/spreadsheets/d/1eVNs1138GhgBzxd4XLqysS_7afdTni9eyuVGDFQX_H4
 export sigGoogle=https://docs.google.com/spreadsheets/d/1T6z2NZ4mc69YllZOXE5TnT5Ey-FlVtaXN1oQ4AIMp7g
 
