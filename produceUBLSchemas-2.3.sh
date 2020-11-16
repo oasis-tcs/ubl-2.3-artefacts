b3cae0f167d4fc094b2eb63dc4f58401d3ba337b
gkholman <gkenholman+github@gmail.com>
2020-11-15 20:29:52 -0500

    Prepare for CSD04

--- produceUBLSchemas-2.3.sh
+++ produceUBLSchemas-2.3.sh
@@ -10,13 +10,13 @@ export label=$3
 export title="UBL 2.3"
 export package=UBL-2.3
 export UBLversion=2.3
-export UBLstage=csd03
+export UBLstage=csd04
 export UBLprevStageVersion=2.3
-export UBLprevStage=csprd02
+export UBLprevStage=csd03
 export UBLprevVersion=2.2
 export rawdir=raw
-export libGoogle=https://docs.google.com/spreadsheets/d/1WBi25tYyXcdQkMNrqVYtQnXuBRIJ9xeHGUrgJQwF6tg
-export docGoogle=https://docs.google.com/spreadsheets/d/1TeTBDJdFtDFju-cBU97YIaGv7yli1V0MYIXn9L4QITM
+export libGoogle=https://docs.google.com/spreadsheets/d/1eilb2NOKIuiy5kzCpp5O8vbZZk-oU9Ao7TiswJmVsvs
+export docGoogle=https://docs.google.com/spreadsheets/d/1_YamjYiJ5DFnWiA5h1tASDCOBTBSOgMYcS5ffHPFa3g
 export sigGoogle=https://docs.google.com/spreadsheets/d/1T6z2NZ4mc69YllZOXE5TnT5Ey-FlVtaXN1oQ4AIMp7g
 
 bash produceUBLSchemas-common.sh "$1" "$2" "$3" "$4"
